774540c5b67083bf541c743698997bc3569eecfd
aamrits <amritanand888@gmail.com>
2019-05-28 22:52:07 +0530

    Updates

---
+++ precache-manifest.596732f3da89d1e5daf06bff7e037b8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d88ecabd80bd032e494c6fe34783a2",
+    "url": "/javascript-tutorial/index.html"
+  },
+  {
+    "revision": "86e1bc3f2a7af1629929",
+    "url": "/javascript-tutorial/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f6d653128d7a60d018a5",
+    "url": "/javascript-tutorial/static/css/main.2b341a38.chunk.css"
+  },
+  {
+    "revision": "86e1bc3f2a7af1629929",
+    "url": "/javascript-tutorial/static/js/2.f859d1a2.chunk.js"
+  },
+  {
+    "revision": "f6d653128d7a60d018a5",
+    "url": "/javascript-tutorial/static/js/main.5bb0a490.chunk.js"
+  },
+  {
+    "revision": "15d4798e85e22dca65bd",
+    "url": "/javascript-tutorial/static/js/runtime~main.93a9789f.js"
+  }
+]);
