825293cca9ccc5096d9e8c130b417eeb8096185b
aamrits <amritanand888@gmail.com>
2019-07-21 16:27:08 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/javascript-tutorial/precache-manifest.20c6122eaa48a7035c582ab517a0a5cd.js"
+  "/javascript-tutorial/precache-manifest.42a597e8113c48a90982e143b49a952d.js"
 );
 
 self.addEventListener('message', (event) => {
